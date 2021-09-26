677fd9e8bcd72e277047f7aa4f62038857d2a123
tashfeen rao <tashfeen@ricult.com>
2021-09-26 18:16:02 +0500

    content projection is complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContentProjectionComponent } from './advanced-components/content-projection/content-projection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentProjectionComponent
   ],
   imports: [
     BrowserModule
